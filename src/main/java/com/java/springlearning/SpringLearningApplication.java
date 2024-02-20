8fc962a1b44097f15907d09c444ccf14a524f5c4
BANVIENCORP\hoang.ngo <hoang.ngo@banvien.com.vn>
2024-02-20 20:15:17 +0700

    Spring in Action, Sixth Edition

---
+++ src/main/java/com/java/springlearning/SpringLearningApplication.java
@@ -0,0 +1,15 @@
+package com.java.springlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearningApplication.class, args);
+    }
+
+}
